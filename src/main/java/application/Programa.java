813331839ef11c163fb6955327c2a7f043ad092e
Luan Pinto <luannelmo1103@gmail.com>
2022-05-07 18:08:13 -0300

    JPA finalizado

--- src/main/java/application/Programa.java
+++ src/main/java/application/Programa.java
@@ -1,17 +1,27 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@email.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquim@email.com");
-		Pessoa p3 = new Pessoa(3, "Emma B.", "emma@email.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@email.com");
+		Pessoa p2 = new Pessoa(null, "Joaquim Torres", "joaquim@email.com");
+		Pessoa p3 = new Pessoa(null, "Emma B.", "emma@email.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Pronto");
 	}
 
 }
